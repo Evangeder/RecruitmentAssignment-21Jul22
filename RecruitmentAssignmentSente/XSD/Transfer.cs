cdf07715f08f5dbbeb8aa7e7485d1254b3f6e5a2
Bartek Szolc <Evangeder@gmail.com>
2022-07-21 02:15:31 +0200

    Added XML Schema Definition and it's respective classes
    XSD's were generated from sample XML files.
    Classes were generated from XSD's and manually edited to have correct syntax in code.

---
+++ RecruitmentAssignmentSente/XSD/Transfer.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Serialization;
+
+namespace XSD
+{
+    [XmlType(AnonymousType = true)]
+    [XmlRoot("przelew", Namespace = "", IsNullable = true)]
+    public partial class Transfer
+    {
+        private int fromField;
+        private int amountField;
+
+        [XmlAttribute("od")]
+        public int From
+        {
+            get => fromField;
+            set => fromField = value;
+        }
+
+        [XmlAttribute("kwota")]
+        public int Amount
+        {
+            get => amountField;
+            set => amountField = value;
+        }
+    }
+
+    [XmlType(AnonymousType = true)]
+    [XmlRoot("przelewy", Namespace = "", IsNullable = true)]
+    public partial class Transfers
+    {
+        private Transfer[] transferField;
+
+        [XmlElement("przelew")]
+        public Transfer[] Transfer
+        {
+            get => transferField;
+            set => transferField = value;
+        }
+    }
+}
