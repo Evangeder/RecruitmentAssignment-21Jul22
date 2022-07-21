4bd095202e21d11fe1cc3c25a19c0158420b0715
Bartek Szolc <Evangeder@gmail.com>
2022-07-21 02:16:29 +0200

    Added Xml Reader based on XSD Classes

---
+++ RecruitmentAssignmentSente/XmlReader.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace RecruitmentAssignmentSente
+{
+    class XmlReader<T> where T : class
+    {
+        private string _filePath;
+
+        public XmlReader(string filePath)
+            => _filePath = filePath;
+
+        public T Read()
+        {
+            if (!_filePath.ToLower().EndsWith(".xml"))
+            {
+                throw new InvalidDataException("Expected XML file.");
+            }
+
+            if (!File.Exists(_filePath))
+            {
+                throw new InvalidDataException("Provided file does not exist.");
+            }
+
+            try
+            {
+                using (var fileStream = new FileStream(_filePath, FileMode.Open))
+                {
+                    fileStream.Position = 0;
+                    var xmlSerializer = new XmlSerializer(typeof(T));
+                    var deserializedObject = xmlSerializer.Deserialize(fileStream);
+
+                    if (deserializedObject != null)
+                    {
+                        return (T)deserializedObject;
+                    }
+                }
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                throw new InvalidDataException("Provided file is in a different format than expected.", ex);
+            }
+
+            return null;
+        }
+    }
+}
