517cd665b7a955d854bafdb00e081d825f2c9137
Bartek Szolc <Evangeder@gmail.com>
2022-07-21 02:28:02 +0200

    Added unit tests for DataManager

--- RecruitmentAssignmentSente/DataManager.cs
+++ RecruitmentAssignmentSente/DataManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("RecruitmentAssignment_SenteTests")]
+[assembly: InternalsVisibleTo("RecruitmentAssignmentSenteTests")]
 
 namespace RecruitmentAssignmentSente
 {
