41021a489973ae41f4fdc49ffb927379e99dd2cc
Bartek Szolc <Evangeder@gmail.com>
2022-07-21 02:18:55 +0200

    Parsing command line arguments, forcing exceptions in en-Us language, usage info.
    When program gets launched with less than two arguments, it will print out it's usage info and close;
    Exceptions are now forced to throw in en-Us language;

--- RecruitmentAssignmentSente/Program.cs
+++ RecruitmentAssignmentSente/Program.cs
@@ -6,7 +6,27 @@ namespace RecruitmentAssignmentSente
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ForceExceptionsLanguage(language: "en-us");
+
+            if (args == null || args.Length < 2)
+            {
+                PrintInfo();
+                return;
+            }
+
+            var transfersFilePath = args[0];
+            var structureFilePath = args[1];
+
+            Console.Read();
         }
+
+        public static void PrintInfo()
+            => Console.WriteLine($"Usage:\n{GetCurrentProcessFilename()} TransfersFilePath StructureFilePath");
+
+        private static string GetCurrentProcessFilename()
+            => System.IO.Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+
+        private static void ForceExceptionsLanguage(string language)
+            => System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(language);
     }
 }
